0b34ebdd3871449278916555cf1400856cb34c5c
fpendii <144331827+fpendii@users.noreply.github.com>
2025-09-15 00:48:40 +0800

    update sidebar

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -110,6 +110,18 @@
                     Pengguna
                 </a>
 
+                <!-- Pengguna -->
+                <a href="{{ url('/logout') }}"
+                    class="flex items-center px-4 py-3 text-sm font-medium rounded-lg
+                    {{ request()->is('logout') ? 'bg-gray-700 text-white' : 'text-gray-100 hover:bg-gray-700' }}">
+                    <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 mr-3" fill="none" viewBox="0 0 24 24"
+                        stroke="currentColor">
+                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                            d="M5.121 17.804A9 9 0 1112 21a9 9 0 01-6.879-3.196zM12 12a3 3 0 100-6 3 3 0 000 6z" />
+                    </svg>
+                    Logout
+                </a>
+
                 {{-- <!-- Logout -->
                 <form method="POST" action="{{ route('logout') }}">
                     @csrf
